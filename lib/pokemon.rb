b7aa4381eee577b35d1f25a33344f7a08c5c8eda
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-02-29 03:26:29 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -23,5 +23,9 @@ class Pokemon
 
     pokemon_inst = Pokemon.new(id: id, name: name, type: type, hp: hp, db: database_connection)
   end  
+
+  def alter_hp(new_hp, database_connection)
+    database_connection.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, @id)
+  end
   
 end
